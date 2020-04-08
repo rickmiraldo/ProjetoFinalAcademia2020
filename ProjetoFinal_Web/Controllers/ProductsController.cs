376257aeda92ccad97177c3622a044b72c756e62
VASCONCELOS NEVES João Pedro <joaopedro.vasconcelosneves@altran.com>
2020-04-08 14:35:18 +0100

    Create View for Product Delete

--- ProjetoFinal_Web/Controllers/ProductsController.cs
+++ ProjetoFinal_Web/Controllers/ProductsController.cs
@@ -52,11 +52,28 @@ namespace ProjetoFinal_Web.Controllers
             return View();
         }
 
-        [HttpDelete]
-        public async Task<IActionResult> Delete(int id)
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteAction(int id)
         {
             await productService.DeleteAsync(id);
             return RedirectToAction("Index");
+        }
+
+        // RETURNS DELETE PAGE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await productService.GetByIdAsync(id.Value);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
         }
 
         [HttpPut]
