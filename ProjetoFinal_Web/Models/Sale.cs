2949a9942515d5f40071f21c7f210083a38331af
Luís <lfilipems@hotmail.com>
2020-04-09 11:58:45 +0100

    Fix merge conflicts

--- ProjetoFinal_Web/Models/Sale.cs
+++ ProjetoFinal_Web/Models/Sale.cs
@@ -14,8 +14,6 @@ namespace ProjetoFinal_Web.Models
         public DateTime SaleDate { get; set; } = DateTime.Now;
 
         [Display(Name = "Total Value" )]
-
-        [Display(Name = "Total Value")]
         public double TotalValue { get; set; }
 
         [Display(Name = "Client Name")]
