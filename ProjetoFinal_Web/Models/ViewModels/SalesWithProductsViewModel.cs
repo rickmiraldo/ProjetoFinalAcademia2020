3e803124368268c60625ed129729bcd48ca65b11
VASCONCELOS NEVES João Pedro <joaopedro.vasconcelosneves@altran.com>
2020-04-09 10:41:16 +0100

    Create Index and Details View for Sales. Create SalesWithProductsViewModel.

---
+++ ProjetoFinal_Web/Models/ViewModels/SalesWithProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjetoFinal_Web.Models.ViewModels
+{
+    using System.Collections.Generic;
+
+    public class SalesWithProductsViewModel
+    {
+        public Sale Sale { get; set; }
+
+        public List<Product> Products { get; set; }
+
+
+    }
+}
