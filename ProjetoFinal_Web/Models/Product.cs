f02e51d7ff47696aeb134e55362848a95088bbdb
Kat Hora <kathorahora@gmail.com>
2020-04-09 10:38:58 +0100

    Improve Views

--- ProjetoFinal_Web/Models/Product.cs
+++ ProjetoFinal_Web/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using ProjetoFinal_API.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,13 @@ namespace ProjetoFinal_Web.Models
     public class Product
     {
         public int ProductId { get; set; }
+
+        [Display(Name = "Product Name")]
         public string ProductName { get; set; }
+
+        [Display(Name = "Unit Price")]
         public double UnitPrice { get; set; }
+
         public int Stock { get; set; }
 
         public ICollection<SaleProduct> SaleProduct { get; set; } = new List<SaleProduct>();
